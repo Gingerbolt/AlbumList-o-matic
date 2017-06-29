c0bee22b2e6d5dfa05e607e0fa8ce5e39aedd95a
Rena Rallis and Steve Spitz <rena@>
2017-06-29 14:26:15 -0700

    debug issues, continue working on creating a search field for the user to search within a cd library

--- app/app.php
+++ app/app.php
@@ -1,33 +1,50 @@
 <?php
-    date_default_timezone_set("America/Los_Angeles");
-    require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/Src.php";
+date_default_timezone_set("America/Los_Angeles");
+require_once __DIR__."/../vendor/autoload.php";
+require_once __DIR__."/../src/Src.php";
 
-    session_start();
+session_start();
 
-    if(empty($_SESSION['list_of_cds'])) {
-      $_SESSION['list_of_cds'] = array();
-    }
 
-    $app = new Silex\Application();
 
-    $app->register(new Silex\Provider\TwigServiceProvider(), array("twig.path" => __DIR__."/../views"
-    ));
+if(empty($_SESSION['list_of_cds'])) {
+    $_SESSION['list_of_cds'] = array();
+}
 
-    $app->get("/", function() use ($app) {
-        return $app["twig"]->render("cdhome.html.twig", array("cds" => CD::getAll()));
-    });
-    $app->post("/cdsave", function() use ($app) {
-      $cd = new CD($_POST['album'], $_POST['artist']);
-      $cd->save();
-      return $app['twig']->render('save_cd.html.twig', array('newcd' => $cd));
-    });
+$app = new Silex\Application();
 
-    $app->post("/delete_list", function() use ($app) {
-      CD::deleteAll();
-      return $app['twig']->render('delete_list.html.twig');
-    });
+$app->register(new Silex\Provider\TwigServiceProvider(), array("twig.path" => __DIR__."/../views"
+));
 
-    return $app;
+$app->post("/delete_list", function() use ($app) {
+    CD::deleteAll();
+    return $app['twig']->render('delete_list.html.twig');
+});
+
+$app->post("/search", function () use ($app) {
+    $_SESSION['albumsearch'] = $_POST['albumsearch'];
+    var_dump($_SESSION['albumsearch']);
+ array_search($_SESSION['albumsearch'], $_SESSION['list_of_cds']);
+    return
+     $app["twig"]->render("search.html.twig",  array("albumresults" => ));
+
+});
+
+$app->get("/search", function () use ($app) {
+    return
+    $app["twig"]->render("search.html.twig" , array("albumresults" => CD::albumSearch()));
+});
+
+$app->get("/", function () use ($app) {
+    return $app["twig"]->render("cdhome.html.twig", array("cds" => CD::getAll()));
+});
+
+$app->post("/cdsave", function() use ($app) {
+    $cd = new CD($_POST['album'], $_POST['artist']);
+    $cd->save();
+    return $app['twig']->render('save_cd.html.twig', array('newcd' => $cd));
+});
+
+return $app;
 
 ?>
--- src/Src.php
+++ src/Src.php
@@ -1,32 +1,39 @@
 <?php
-    class CD
+class CD
+{
+    private $album_title;
+    private $artist_name;
+    private $album_cover_art;
+
+    function __construct($album_title, $artist_name)
     {
-        private $album_title;
-        private $artist_name;
-        private $album_cover_art;
-
-        function __construct($album_title, $artist_name)
-        {
-            $this->album_title = $album_title;
-            $this->artist_name = $artist_name;
-            $this->album_cover_art;
-        }
-
-        function getAlbumTitle() {
-            return $this->album_title;
-        }
-        function getArtistTitle() {
-            return $this->artist_name;
-        }
-        static function getAll() {
-            return $_SESSION['list_of_cds'];
-        }
-        function save()
-        {
-            array_push($_SESSION['list_of_cds'], $this);
-        }
-        static function deleteAll()
-        {
-            return $_SESSION['list_of_cds'] = array();
-        }
+        $this->album_title = $album_title;
+        $this->artist_name = $artist_name;
+        $this->album_cover_art;
     }
+
+    function getAlbumTitle() {
+        return $this->album_title;
+    }
+    function getArtistTitle() {
+        return $this->artist_name;
+    }
+    static function getAll() {
+        return $_SESSION['list_of_cds'];
+    }
+    function save()
+    {
+        array_push($_SESSION['list_of_cds'], $this);
+    }
+    static function deleteAll()
+    {
+        return $_SESSION['list_of_cds'] = array();
+    }
+    static function albumSearch()
+    {
+        return array_search($_SESSION['albumsearch'], $this);
+    }
+
+}
+
+?>
