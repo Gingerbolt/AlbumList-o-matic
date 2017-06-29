0989560186ece3a61e230d5f54441852577f0b28
Rena Rallis and Steve Spitz <steve@>
2017-06-29 10:00:51 -0700

    Add minimum product for taking and returning artist and album information.

--- app/app.php
+++ app/app.php
@@ -5,11 +5,29 @@
 
     session_start();
 
+    if(empty($_SESSION['list_of_cds'])) {
+      $_SESSION['list_of_cds'] = array();
+    }
+
     $app = new Silex\Application();
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array("twig.path" => __DIR__."/../views"
-));
+    ));
 
-return $app;
+    $app->get("/", function() use ($app) {
+        return $app["twig"]->render("cdhome.html.twig", array("cds" => CD::getAll()));
+    });
+    $app->post("/cdsave", function() use ($app) {
+      $cd = new CD($_POST['album'], $_POST['artist']);
+      $cd->save();
+      return $app['twig']->render('save_cd.html.twig', array('newcd' => $cd));
+    });
+
+    $app->post("/delete_list", function() use ($app) {
+      CD::deleteAll();
+      return $app['twig']->render('delete_list.html.twig');
+    });
+
+    return $app;
 
 ?>
---
+++ src/Src.php
@@ -0,0 +1,32 @@
+<?php
+    class CD
+    {
+        private $album_title;
+        private $artist_name;
+        private $album_cover_art;
+
+        function __construct($album_title, $artist_name)
+        {
+            $this->album_title = $album_title;
+            $this->artist_name = $artist_name;
+            $this->album_cover_art;
+        }
+
+        function getAlbumTitle() {
+            return $this->album_title;
+        }
+        function getArtistTitle() {
+            return $this->artist_name;
+        }
+        static function getAll() {
+            return $_SESSION['list_of_cds'];
+        }
+        function save()
+        {
+            array_push($_SESSION['list_of_cds'], $this);
+        }
+        static function deleteAll()
+        {
+            return $_SESSION['list_of_cds'] = array();
+        }
+    }
